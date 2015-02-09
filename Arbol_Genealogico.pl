4c4906dcfca9807251090a0650b5fe5017e6971e
Angelica Barreda B. <anjelyk.b@gmail.com>
2015-02-08 23:08:25 -0600

    Fisrt
    The first relation in the members of family

---
+++ Arbol_Genealogico.pl
@@ -0,0 +1,15 @@
+esPadre(juan,jose).
+esMadre(carmen,jose).
+
+esPadre(angelica,eliseo).
+esPadre(angelica,graciela).
+esPadre(javier,graciela).
+esMadre().
+esMadre().
+esMadre().
+
+esPadre().
+esMadre().
+
+esPadre().
+
